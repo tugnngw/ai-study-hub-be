2b227e6df46771ff2456e24a256435490798d662
tugnngw <tugnngw@gmail.com>
2026-06-02 16:10:10 +0700

    Start project

---
+++ src/main/java/com/tugnw/aistudy/AiStudyHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.tugnw.aistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyHubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiStudyHubBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tugnw/aistudy/AiStudyHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugnw.aistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyHubBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
